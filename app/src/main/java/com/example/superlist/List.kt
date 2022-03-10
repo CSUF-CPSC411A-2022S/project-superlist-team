7009ea0ae9fdd60839a48c904799a3f2f211e466
Chivista008 <chivista008@csu.fullerton.edu>
2022-03-09 16:54:40 -0800

    Added blank Functions and a new class.

--- app/src/main/java/com/example/superlist/List.kt
+++ app/src/main/java/com/example/superlist/List.kt
@@ -1,9 +1,58 @@
 package com.example.superlist
 
-class List<T> {
-    fun <T> listOf() {
+class Food_List<T> {
+    fun <T> list() {
         val list = listOf<String>()
         println(list)
     }
+    fun <T> remove_item() {
+
+    }
+
+    fun <T> add_item() {
+    }
+
+    fun <T> cost() {
+    }
+
+    fun <T> price() {
+
+    }
+
+    fun<T> calories() {
+
+    }
+    fun <T> price_sum() {
+
+    }
+
+
+}
+
+class Item_List<T> {
+    fun <T> list() {
+        val list = listOf<String>()
+        println(list)
+    }
+    fun <T> remove_item() {
+
+    }
+
+    fun <T> add_item() {
+    }
+
+    fun <T> cost() {
+    }
+
+    fun <T> price() {
+
+    }
+
+    fun <T> price_sum() {
+
+    }
+
+
+
 }
 
