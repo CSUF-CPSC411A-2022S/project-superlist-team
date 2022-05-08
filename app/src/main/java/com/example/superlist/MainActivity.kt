e684a57a608f5270a04e9e7fa517b86f22cd64c6
Chivista008 <54595486+Chivista008@users.noreply.github.com>
2022-05-08 15:51:37 -0700

    Update MainActivity.kt

--- app/src/main/java/com/example/superlist/MainActivity.kt
+++ app/src/main/java/com/example/superlist/MainActivity.kt
@@ -9,12 +9,27 @@ import com.example.superlist.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        setContentView(R.layout.activity_main)
+        
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val navController = this.findNavController(R.id.navigation)
         NavigationUI.setupActionBarWithNavController(this,navController)
 
+        
+        val btn_click = findViewById<Button>(R.id.add)
+        val delete_button = findViewById<Button>(R.id.delete)
+        val clear_button = findViewById<Button>(R.id.clear)
+
+        btn_click.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Adding", Toast.LENGTH_SHORT).show()
+        }
+        delete_button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Deleting", Toast.LENGTH_SHORT).show()
+        }
+        clear_button.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Clearing", Toast.LENGTH_SHORT).show()
+        }
     }
 }
