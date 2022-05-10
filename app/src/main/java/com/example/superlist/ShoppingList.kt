e7e5ab24c41fcc121e06cdd5b1932de83d506443
quentinmay <quentinemay@gmail.com>
2022-05-09 23:21:15 -0700

    Retrofit + moshi api implementations for food api.

--- app/src/main/java/com/example/superlist/ShoppingList.kt
+++ app/src/main/java/com/example/superlist/ShoppingList.kt
@@ -1,13 +1,16 @@
 package com.example.superlist
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-import com.example.superlist.databinding.ActivityMainBinding
 import com.example.superlist.databinding.FragmentShoppingListBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 
 class ShoppingList : Fragment() {
@@ -26,6 +29,38 @@ class ShoppingList : Fragment() {
             view.findNavController()
                 .navigate(R.id.action_shoppingList_to_itemDisplay)
         }
+
+
+//        Food.FoodAPI.Api.retrofitService.getProductByName("chicken noodle soup").enqueue(
+//            object : javax.security.auth.callback.Callback, Callback<ProductSearch> {
+//                override fun onResponse(call: Call<ProductSearch>, response: Response<ProductSearch>) {
+//                    // We can access the properties of the Place object, but use safe calls
+//                    // to avoid issues.
+//                    System.out.println("getProductByName succeeded")
+//                    Log.d("getProductByName", response.toString())
+//                    println("${response.body()?.type} ${response.body()?.products?.get(0)?.title} ${response.body()?.products?.get(0)?.image}")
+//                }
+//
+//                override fun onFailure(call: Call<ProductSearch>, t: Throwable) {
+//                    println("Failure ${t.message}")
+//
+//                }
+//            })
+
+//        Food.FoodAPI.Api.retrofitService.getNutrition("1003464").enqueue(
+//            object : javax.security.auth.callback.Callback, Callback<Nutrition> {
+//                override fun onResponse(call: Call<Nutrition>, response: Response<Nutrition>) {
+//                    System.out.println("asdf1")
+//                    println("${response.body()?.carbs} ${response.body()?.fat} ${response.body()?.protein}")
+//                }
+//
+//                override fun onFailure(call: Call<Nutrition>, t: Throwable) {
+//                    println("Failure ${t.message}")
+//
+//                }
+//            })
+
+
         return inflater.inflate(R.layout.fragment_shopping_list, container, false)
     }
 }
